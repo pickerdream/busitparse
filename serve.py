fbd395004f2aec3fe682b06075d4a4b0d917f5d1
pickerdream <59798480+pickerdream@users.noreply.github.com>
2025-12-28 16:31:11 +0900

    update

--- serve.py
+++ serve.py
@@ -2,20 +2,13 @@ import gettable
 from flask import Flask, request, jsonify
 from urllib.parse import urlparse
 
-def is_valid_url(url):
-    try:
-        result = urlparse(url)
-        return all([result.scheme, result.netloc])
-    except:
-        return False
-
 app = Flask(__name__)
 @app.route('/timetable.json', methods=['GET'])
 def get_timetable():
     target_url = request.args.get('url')
     if not target_url:
         return jsonify({"error": "URL parameter is required"}), 400
-    if not is_valid_url(target_url):
+    if not gettable.is_valid_url(target_url):
         return jsonify({"error": "URL parameter is invaild"}), 400
     else:
         return jsonify(gettable.main(target_url))
